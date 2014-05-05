1eb1c54c3d7ce2e1f4ed9f599246baf89a6f4692
Ethan White <ethan@weecology.org>
2014-05-05 09:06:08 -0400

    Remove opening comment

--- R/ecoretriever.r
+++ R/ecoretriever.r
@@ -1,23 +1,3 @@
-#' Download public datasets via the EcoData Retriever.
-#'
-#' Data is stored in either CSV files or one of the following database management
-#' systems: MySQL, PostgreSQL, SQLite, or Microsoft Access.
-#'
-#' @param datasets the names of the datasets that you wish to download
-#' @param connection what type of database connection should be used. 
-#' The options include: mysql, postgres, sqlite, msaccess, or csv'
-#' @param db_file the name of the datbase file the dataset should be loaded 
-#' into
-#' @param log_dir the location where the retriever log should be stored if 
-#' the progress is not printed to the console
-#' @param user the user argument for connecting data to a database server
-#' @param pwd the password argument for connecting data to a database server
-#' @param host the host argument for connecting data to a database server
-#' @param port the port argument for connecting data to a database server
-#' @export
-#' @examples
-#' install_data('MCDB', 'csv')
-
 install_data = function(dataset, connection, db_file=NULL,
                                 log_dir=NULL, user=NULL, pwd=NULL, host=NULL,
                                 port=NULL){
